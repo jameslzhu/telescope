6f820bc375b5be6675b9ee32eb215b2fb4831176
James Zhu <james.zhu.engineer@gmail.com>
2016-05-03 12:20:33 -0500

    Minor tweaks

--- src/atom.rs
+++ src/atom.rs
@@ -1,7 +1,6 @@
 #![allow(dead_code)]
 
 use std::fmt;
-use std::slice;
 use std::iter;
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
@@ -32,6 +31,12 @@ pub enum Atom<T> {
     Sym(Sym),
 }
 
+impl<T> From<Sym> for Atom<T> {
+    fn from(s: Sym) -> Self {
+        Atom::Sym(s)
+    }
+}
+
 impl<T> fmt::Display for Atom<T> where T: fmt::Display
 {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -49,10 +54,6 @@ pub enum Expr<T> {
     List(Vec<Expr<T>>),
 }
 
-pub type Iter<'a, T> = slice::Iter<'a, Expr<T>>;
-pub type IterMut<'a, T> = slice::IterMut<'a, Expr<T>>;
-pub type IntoIter<'a, T> = Iter<'a, T>;
-
 impl<T> Expr<T> {
     fn is_atom(&self) -> bool {
         use self::Expr::*;
@@ -65,8 +66,8 @@ impl<T> Expr<T> {
     fn is_list(&self) -> bool {
         use self::Expr::*;
         match *self {
-            Atom(_) => true,
-            List(_) => false,
+            Atom(_) => false,
+            List(_) => true,
         }
     }
 }
@@ -95,12 +96,7 @@ impl<T> fmt::Display for Expr<T> where T: fmt::Display
     }
 }
 
-#[test]
-fn test_display()
-{
-    assert_eq!("1", Expr::Atom(Atom::Num(1)).to_string());
-    assert_eq!("( 1 2 )", Expr::<i32>::from(vec![1, 2]).to_string());
-}
+
 
 impl<T> iter::FromIterator<Expr<T>> for Expr<T> {
     fn from_iter<U>(iterator: U) -> Self
@@ -115,5 +111,17 @@ impl<T> iter::FromIterator<Atom<T>> for Expr<T> {
         where U: IntoIterator<Item = Atom<T>>
     {
         Expr::List::<T>(iterator.into_iter().map(Expr::Atom).collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_display()
+    {
+        assert_eq!("1", Expr::from(1).to_string());
+        assert_eq!("( 1 2 )", Expr::<i32>::from(vec![1, 2]).to_string());
     }
 }
