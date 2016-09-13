77f279a4949cf3c503721836dd84744026e71d46
James Zhu <james.zhu.engineer@gmail.com>
2016-09-12 22:31:15 -0700

    Use &[Value] in ops instead of &Vec<Value>
    Allows for more generic slices.

--- src/ops.rs
+++ src/ops.rs
@@ -1,26 +1,26 @@
 use atom::*;
 use error::*;
 
-pub fn add(v: &Vec<Value>) -> Result<Value> {
+pub fn add(v: &[Value]) -> Result<Value> {
     assert!(v.len() >= 2);
     v.iter().fold(Ok(0.into()), |acc, x| acc.and_then(|a| a.add(x)))
 }
 
-pub fn sub(v: &Vec<Value>) -> Result<Value> {
+pub fn sub(v: &[Value]) -> Result<Value> {
     assert!(v.len() == 2);
     v[0].sub(&v[1])
 }
-pub fn mul(v: &Vec<Value>) -> Result<Value> {
+pub fn mul(v: &[Value]) -> Result<Value> {
     assert!(v.len() >= 2);
     v.iter().fold(Ok(1.into()), |acc, x| acc.and_then(|a| a.mul(x)))
 }
 
-pub fn div(v: &Vec<Value>) -> Result<Value> {
+pub fn div(v: &[Value]) -> Result<Value> {
     assert!(v.len() >= 2);
     v[0].div(&v[1])
 }
 
-pub fn modulus(v: &Vec<Value>) -> Result<Value> {
+pub fn modulus(v: &[Value]) -> Result<Value> {
     assert!(v.len() == 2);
     v[0].modulus(&v[1])
 }
