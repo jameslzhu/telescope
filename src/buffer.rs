3c6d90e6cea695c3746c687c0ec7cf4c931b290e
James Zhu <james.zhu.engineer@gmail.com>
2017-07-17 22:57:55 -0500

    Separate buffer into readline, buffer (generic over Read)

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,37 +1,33 @@
-use std::io::prelude::*;
 use std::io;
-use rustyline::Editor;
+use std::io::prelude::*;
+use rustyline;
 use rustyline::error::ReadlineError as RLError;
-use combine::{StreamOnce};
+use combine;
 use error::*;
 
-#[derive(Debug)]
-pub struct Buffer {
-    buffer: String,
+pub struct Readline {
+    rl: rustyline::Editor<()>,
     prompt: String,
-    rl: Editor<()>,
+    buffer: Vec<u8>,
 }
 
-impl Buffer {
-    fn new<S>(prompt: S) -> Self
-    where
-        S: Into<String>
-    {
-        Buffer {
-            buffer: String::with_capacity(128),
-            prompt: prompt.into(),
-            rl: Editor::new(),
+impl Readline {
+    fn new<S>(prompt: &str) -> Self {
+        Readline {
+            rl: rustyline::Editor::new(),
+            prompt: prompt.to_owned(),
+            buffer: Vec::with_capacity(128),
         }
     }
 }
 
-impl Read for Buffer {
+impl Read for Readline {
     fn read(&mut self, mut buf: &mut [u8]) -> io::Result<usize> {
         if self.buffer.is_empty() {
             match self.rl.readline(&self.prompt) {
                 Ok(line) => {
                     self.rl.add_history_entry(&line);
-                    self.buffer.push_str(&line);
+                    self.buffer.write(line.as_bytes());
                 },
                 Err(RLError::Io(err)) => return Err(err),
                 Err(RLError::Interrupted) => return Err(io::ErrorKind::Interrupted.into()),
@@ -43,22 +39,45 @@ impl Read for Buffer {
 
         // if buf > buffer, drain entire buffer
         if buf.len() >= self.buffer.len() {
-            let result = buf.write(self.buffer.as_bytes());
+            let result = buf.write(&self.buffer);
             self.buffer.clear();
             result
         } else {
-            let bytes = self.buffer.drain(0..buf.len()).collect::<String>();
-            buf.write(bytes.into_bytes().as_slice())
+            let drained = self.buffer.drain(0..buf.len()).collect::<Vec<_>>();
+            buf.write(&drained)
         }
     }
 }
 
-impl BufRead for Buffer {
+impl io::BufRead for Readline {
     fn fill_buf(&mut self) -> io::Result<&[u8]> {
-
+        unimplemented!()
     }
 
     fn consume(&mut self, amt: usize) {
-
+        unimplemented!()
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct StringStream<R: io::Read> {
+    src: R,
+    buffer: String,
+}
+
+impl<R: io::Read> StringStream<R> {
+    fn new(src: R) -> Self {
+        StringStream {
+            src: src,
+            buffer: String::with_capacity(128),
+        }
+    }
+}
+
+impl<R: io::Read> combine::StreamOnce for StringStream<R> {
+    type Item = char;
+    type Range = str;
+    type Position usize;
+    fn uncons(&mut self) -> Result<Self::Item, Error<Self::Item, Self::Range>>;
+    fn position(&self) -> Self::Position;
 }
